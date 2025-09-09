11b4daadc5b5a4acd7d3a85c469cbc223a00c1d7
PierKnight <pier.altimari@libero.it>
2025-09-09 05:53:46 -0400

    Refactoring and WhoIs Collection with Throttling

--- src/typosniffer/cli.py
+++ src/typosniffer/cli.py
@@ -5,6 +5,7 @@ from dataclasses import asdict
 import json
 import click
 from typosniffer.sniffing import sniffer
+from typosniffer.sniffing.whoisfinder import find_whois
 from typosniffer.utils.console import console
 from typosniffer.config import config
 from typosniffer.fuzzing import fuzzer
@@ -125,17 +126,17 @@ def sniff(tld_dictionary: list[str], word_dictionary: list[str], nameservers: li
             with open(output, "w") as f:
                 json.dump(results, f, indent=4)
 
-@cli.command(help = "Clean old whoisds domains")
+@cli.command(help = "Clear old whoisds domains")
 @click.argument('days',type=click.IntRange(min=1))
-def clean(days: int):
-    with console.status("[bold green]Cleaning old Domains[/bold green]"):
-        removed_files = whoisds.clean_old_domains(max_days=days)
-    console.print(f"[bold green]Cleaned {removed_files} old domains[/bold green]")
+def clear(days: int):
+    with console.status("[bold green]Clearing old Domains[/bold green]"):
+        removed_files = whoisds.clear_old_domains(max_days=days)
+    console.print(f"[bold green]Cleared {removed_files} old domains[/bold green]")
 
 
 @cli.command(help = "Update And Scan Domains collected from whoisds.com")
 @click.option('-d', '--days', type=click.IntRange(min=1), default=30, help='Max days to update whoisds files')
-@click.option('-c', '--clean-days', type=click.IntRange(min=0), default=0, help='Clean whoisds domains older that this number of days')
+@click.option('-c', '--clear-days', type=click.IntRange(min=0), default=0, help='Clear whoisds domains older that this number of days')
 @click.argument('domain', callback=utility.validate_regex(VALID_FQDN_REGEX, "not valid domain"))
 @click.option('--dameraulevenshtein', type=click.IntRange(min=0), default=None, help='Override default dameraulevenshtein.')
 @click.option('--hamming', type=click.IntRange(min=0), default=None, help='Override default hamming.')
@@ -145,7 +146,7 @@ def clean(days: int):
 @click.option('-f', '--format', type=click.Choice(['csv', 'json'], case_sensitive=False), default='json', help='format of output file')
 def scan(
     days: int,
-    clean_days: int,
+    clear_days: int,
     domain: str,
     dameraulevenshtein: int, 
     hamming: int,
@@ -161,7 +162,7 @@ def scan(
         levenshtein=levenshtein if levenshtein is not None else sniffer.DEFAULT_CRITERIA.levenshtein,
     )
 
-    sniff_result = whoisds.whoisds_cli(domain, update_days=days, max_days=clean_days, criteria=criteria)
+    sniff_result = whoisds.whoisds_cli(domain, update_days=days, max_days=clear_days, criteria=criteria)
 
     if output:
         if format == "json":
@@ -173,16 +174,17 @@ def scan(
                 writer.writerow(["domain", "dameraulevenshtein", "hamming", "jaro", "levenshtein"])
                 for sniff in sniff_result:
                     writer.writerow([sniff.domain, sniff.dameraulevenshtein, sniff.hamming, sniff.jaro, sniff.levenshtein])
-            
-
-
-
-@cli.command(help = "Updates the tld dictionary from iana.org to generate all possible permutation using the fuzzer")
-
-def tld():
-    with console.status("[bold green]Updating tdl File[/bold green]"):
-        fuzzer.update_tld_dictionary()
     
+    with console.status("[bold green]Retrieving whois data[/bold green]"):
+        print(find_whois([sniff.domain for sniff in sniff_result]))
+
+            
+    
+
+@cli.command
+def test():
+    print(find_whois(["google.com"]))
+
 
                                     
 if __name__ == "__main__":
