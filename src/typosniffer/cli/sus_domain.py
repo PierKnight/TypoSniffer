b6f60c16175ddde1b98b58bb89ac99e6cda25e5d
PierKnight <pier.altimari@libero.it>
2025-09-16 11:48:34 +0200

    Use SuspiciousDomainDto, Fixed Not Working Screenshot Feature,

--- src/typosniffer/cli/sus_domain.py
+++ src/typosniffer/cli/sus_domain.py
@@ -2,6 +2,7 @@
 
 import click
 
+from typosniffer.config.config import get_config
 from typosniffer.service import suspicious_domain
 from typosniffer.sniffing.monitor import monitor_domains
 from typosniffer.utils import console
@@ -23,5 +24,6 @@ def monitor(max_workers: int):
         console.print_info("No suspicious domains have been found: use 'typosniffer scan' to update the list")
         return
 
+    cfg = get_config()
 
-    monitor_domains(domains, max_workers)
+    monitor_domains(domains, cfg.monitor.max_workers)
